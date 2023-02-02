3e3872b6b2f7e686f4cc113381d44840156fc614
Eric <ericlg.link@gmail.com>
2023-02-01 19:48:21 -0800

    Fixes are happening

--- src/context/CardsContext.tsx
+++ src/context/CardsContext.tsx
@@ -1,34 +1,60 @@
-import { createContext, useReducer } from 'react'
+import * as React from 'react';
 
-export const CardsContext = createContext()
-
-export const cardsReducer = (state: { cards: any[] }, action: { type: any; payload: { _id: any } }) => {
-  switch (action.type) {
-    case 'SET_CARDS': 
-      return {
-        cards: action.payload
-      }
-    case 'CREATE_CARDS':
-      return {
-        cards: [action.payload, ...state.cards]
-      }
-    case 'DELETE_CARD':
-      return {
-        cards: state.cards.filter((w: { _id: any }) => w._id !== action.payload._id)
-      }
-    default:
-      return state
-  }
+function useReducer(_cardsReducer: (state: CardsState, action: CardAction) => CardsState | { cards: { _id: any; }; }, arg1: { cards: any[]; }): [any, any] {
+  throw new Error('Function not implemented.');
 }
 
-export const CardsContextProvider = ({ children }) => {
+export interface CardsState {
+  cards: any[];
+}
+
+export interface CardAction {
+  type: any;
+  payload: { _id: any };
+}
+
+export const CardsContext = React.createContext<{
+  state: CardsState;
+  dispatch: React.Dispatch<CardAction>;
+}>({
+  state: { cards: [] },
+  dispatch: () => null,
+});
+
+export const cardsReducer = (state: CardsState, action: CardAction) => {
+  switch (action.type) {
+    case 'SET_CARDS':
+      return {
+        cards: action.payload,
+      };
+    case 'CREATE_CARDS':
+      return {
+        cards: [action.payload, ...state.cards],
+      };
+    case 'DELETE_CARD':
+      return {
+        cards: state.cards.filter((w) => w._id !== action.payload._id),
+      };
+    default:
+      return state;
+  }
+};
+
+interface CardsContextProviderProps {
+  children: React.ReactNode;
+}
+
+export const CardsContextProvider = ({ children }: CardsContextProviderProps) => {
   const [state, dispatch] = useReducer(cardsReducer, {
-    cards: null
+    cards: [] as any[]
   })
 
   return (
-    <CardsContext.Provider value={{...state, dispatch}}>
-      { children }
+    <CardsContext.Provider value={{ state, dispatch }}>
+      {children}
     </CardsContext.Provider>
-  )
-}
+  );
+};
+
+
+
